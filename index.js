96deb39b9d3889fac48d0d18a939bb8a42bd982e
brocoden <raditstrike@gmail.com>
2024-12-13 14:22:09 +0800

    ubah teks

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("hello world");
+console.log("hello world , semoga dengan saya belajar ini sya menjadi lebbih pintar dari sebelumnya dan selalu belajar dari kesalahan dan jangan pernah ada error lagi");
 console.log("hallo worldddddddddd");
 
 console.log("otw fullstack dev dengan konsisten berdoa , belajar dan terus berusaha , astungkara tercapai");
