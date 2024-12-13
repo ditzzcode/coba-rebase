9199a83587a5acea0ff1c633db2e120556d05ca7
ditzzcode <radityahuuu@gmail.com>
2024-12-13 14:23:11 +0800

    semoga ga conflig lagi

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("hello world");
 console.log("hallo dunia");
 
 console.log("otw fullstack dev dengan konsisten berdoa , belajar dan terus berusaha , astungkara tercapai");
+console.log("plis semoga tidak ada conflig lagi");
